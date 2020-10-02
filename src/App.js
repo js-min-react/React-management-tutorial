960951ba75fc1499fe55d457cabf4f1441c2f3dc
js-min-react <minjaesik1@gmail.com>
2020-10-02 11:22:59 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <h2>Let's develop management system!</h2>
+      <img src="https://app.jjalbang.today/jj1NZ.gif"></img>
+      
+    </div>
+  );
+}
+
+export default App;
